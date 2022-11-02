b675a72ffa93e51095093defc4694944ad93e82d
Muzayyin Al-Afnan Siregar <108184882+AfnanSiregar@users.noreply.github.com>
2022-11-03 03:09:09 +0700

    Update TipsView.vue

--- src/components/TipsView.vue
+++ src/components/TipsView.vue
@@ -2,7 +2,7 @@
     <section class="bg-red-100/30 text-center py-16">
         <div class="flex items-center justify-center flex-col">
             <p class="text-2xl font-semibold text-black">
-                4 Langkah Mudah Mendapatkan Auransi <a href="#" class="text-blue-900">AXA Life Indonesia</a> di Pilihkartu
+                4 Langkah Mudah Mendapatkan Asuransi <a href="#" class="text-blue-900">AXA Life Indonesia</a> di Pilihkartu
             </p>
             <br>
             <img src="../assets/langkah.png" alt="tips" class="w-8/12 h-8/12">
